4829ed2db1ddada7c4c83b029b8f07d7d3b6d038
银月 <2558255419@qq.com>
2018-08-17 17:27:45 +0800

    添加基本文件
    添加基本文件

---
+++ dataCharts/src/components/HelloWorld.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <el-button @click="visible = true">Button</el-button>
+    <i class="el-icon-edit"></i>
+    <i class="el-icon-share"></i>
+    <i class="el-icon-delete"></i>
+    <el-button type="primary" icon="el-icon-search">搜索</el-button>
+    <el-input-number v-model="num1" @change="handleChange" :min="1" :max="10" label="描述文字"></el-input-number>
+    <el-dialog :visible.sync="visible" title="Hello world">
+      <p>Try Element</p>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      visible: false,
+      num1:1
+    }
+  },
+  methods: {
+    handleChange(value) {
+      console.log(value);
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
